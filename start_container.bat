df04b3e326506d5cda7c3f9f92bf100a066c319a
Nicky Amaryllis <146944081+NikkyAS@users.noreply.github.com>
2025-04-22 19:55:03 +0200

    Update start_container.bat

--- start_container.bat
+++ start_container.bat
@@ -1,5 +1,5 @@
 @echo off
-set CONTAINER_NAME=ml-container
+set CONTAINER_NAME=iot-container
 
 :: Check if container exists
 docker ps -a --format "{{.Names}}" | findstr /I /C:"%CONTAINER_NAME%" >nul
@@ -10,5 +10,5 @@ if %errorlevel%==0 (
 ) else (
     echo Container "%CONTAINER_NAME%" findes ikke!
     echo Du skal først oprette den med:
-    echo docker run --name ml-container -v "%cd%:/app" -w /app -it sep4-python-env bash
+    echo docker run --name iot-container -v "%cd%:/app" -w /app -it sep4-java-env bash
 )
