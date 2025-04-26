9bd17925e2a5b8fff059d769c32dfdefabddcea1
Dooneh <dannivesterdal@gmail.com>
2025-04-26 14:25:12 +0200

    Data Listener implementeret. (Midlertidig løsning)
    Mangler logik for videresendelse af data.

---
+++ backend/iot-backend/src/main/java/via/sep4/datalistener/ESPDataService.java
@@ -0,0 +1,14 @@
+package via.sep4.datalistener;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ESPDataService {
+
+    public void processData(String data) {
+
+        System.out.println("Processing data: " + data); //Midlertidig løsning. Skal valideres, og så sendes videre.
+
+
+    }
+}
