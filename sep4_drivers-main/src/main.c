89be9407dbf0a5d7c8f652c564922408e7a41977
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-05-08 10:37:39 +0200

    feat: PIR-sensor virker og registrerer bevægelse

--- sep4_drivers-main/src/main.c
+++ sep4_drivers-main/src/main.c
@@ -3,6 +3,7 @@
 #include "hc_sr04.h"
 #include "display.h"
 #include "dht11.h"
+#include "pir.h"
 #include <util/delay.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +14,12 @@
 static uint8_t _buff[100];
 static uint8_t _index = 0;
 volatile static bool _done = false;
+volatile static bool motion_detected = false;
+
+void pir_motion_detected(void) {
+    motion_detected = true;
+    uart_send_string_blocking(USART_0, "Motion detected!\n");
+}
 
 void console_rx(uint8_t _rx)
 {
@@ -42,6 +49,7 @@ int main()
     hc_sr04_init();
     display_init();
     dht11_init();
+    pir_init(pir_motion_detected);
 
     sei();
 
@@ -50,7 +58,7 @@ int main()
 
     uart_send_string_blocking(USART_0, prompt_text);
 
-    char send_buffer[128]; 
+    char send_buffer[128];
 
     while (1)
     {
@@ -62,16 +70,20 @@ int main()
 
         if (dht_status == DHT11_OK)
         {
-            sprintf(send_buffer, "Distance: %u cm, Temp: %d.%d C, Humidity: %d.%d %%\n",
-                    distance, temp_int, temp_dec, hum_int, hum_dec);
+            sprintf(send_buffer, "Distance: %u cm, Temp: %d.%d C, Humidity: %d.%d %%, Motion: %s\n",
+                    distance, temp_int, temp_dec, hum_int, hum_dec,
+                    motion_detected ? "Yes" : "No");
         }
         else
         {
-            sprintf(send_buffer, "Distance: %u cm, DHT11 sensor error!\n", distance);
+            sprintf(send_buffer, "Distance: %u cm, DHT11 sensor error!, Motion: %s\n",
+                distance, motion_detected ? "Yes" : "No");
         }
 
         wifi_command_TCP_transmit((uint8_t*)send_buffer, strlen(send_buffer));
         uart_send_string_blocking(USART_0, send_buffer);
+
+        motion_detected = false;
 
         _delay_ms(10000); 
     }
